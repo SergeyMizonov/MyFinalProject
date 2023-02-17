969e5d1e623021f780a6eb76bc8ff4b3ff32701a
Sergey <samizunopro@gmail.com>
2023-02-17 13:32:47 +0300

    Создали файл Program

---
+++ Project/Program.cs
@@ -0,0 +1,33 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести 
+// с клавиатуры, либо задать на старте выполнения алгоритма. 
+// При решение не рекомендуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами.
+
+string[] array1;
+Console.Write("Введите массив строк через запятую: ");
+string stroka = Console.ReadLine()!;
+array1 = stroka.Split(',');
+string[] array2 = new string[array1.Length];
+void ShortArray(string[] array1, string[] array2)
+{
+                    int count = 0;
+                    for (int i = 0; i < array1.Length; i++)
+                    {
+                                        if (array1[i].Length <= 3)
+                                        {
+                                                            array2[count] = array1[i];
+                                                            count++;
+                                        }
+                    }
+}
+void PrintArray(string[] array)
+{
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                                        Console.Write($"{array[i]} ");
+                    }
+                    Console.WriteLine();
+}
+ShortArray(array1, array2);
+PrintArray(array2);
